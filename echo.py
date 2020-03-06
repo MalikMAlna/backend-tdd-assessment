6960f66e44b86940be801cfec42031a365013914
Mike A <jimalik2525@gmail.com>
2020-03-06 14:55:15 -0500

    Almost done

--- echo.py
+++ echo.py
@@ -2,21 +2,48 @@
 # -*- coding: utf-8 -*-
 """An enhanced version of the 'echo' cmd line utility"""
 
-__author__ = "???"
+__author__ = "Worked with Ybrayym and watched demo by madarp"
 
 
 import sys
+import argparse
 
 
 def create_parser():
     """Creates and returns an argparse cmd line option parser"""
-    pass
+    parser = argparse.ArgumentParser(
+        description="Perform transformation on input text.")
+    parser.add_argument(
+        '-u', "--upper",
+        action="store_true",
+        help="Takes text input and returns text in uppercase")
+    parser.add_argument(
+        '-l', "--lower",
+        action="store_true",
+        help="Takes text input and returns text in lowercase")
+    parser.add_argument(
+        '-t', "--title",
+        action="store_true",
+        help="Takes text input and returns text in titlecase")
+    parser.add_argument(
+        'text',
+        help="Text to be manipulated")
+    return parser
 
 
-def main(args):
+def main(raw_args):
     """Implementation of echo"""
-    pass
+    parser = create_parser()
+    args = parser.parse_args(raw_args)
+    result = args.text
+    if args.upper:
+        result = result.upper()
+    if args.lower:
+        result = result.lower()
+    if args.title:
+        result = result.title()
+    return result
 
 
 if __name__ == '__main__':
-    pass
+    print(main(sys.argv[1:]))
--- tests/test_echo.py
+++ tests/test_echo.py
@@ -2,11 +2,110 @@
 # -*- coding: utf-8 -*-
 
 import unittest
+import sys
+import subprocess
 import echo
 
 # Your test case class goes here
+
+
 class TestEcho(unittest.TestCase):
-    pass
+    def setUp(self):
+        """This function is called only once for all tests"""
+        self.parser = echo.create_parser()
+        self.pystring = "python2"
+        if sys.version_info[0] == 3:
+            self.pystring = "python3"
+
+
+def test_help(self):
+    """ Running the program without arguments should show usage. """
+
+    # Run the command `python ./echo.py -h` in a separate process, then
+    # collect it's output.
+    process = subprocess.Popen(
+        [self.pystring, "./echo.py", "-h"],
+        stdout=subprocess.PIPE)
+    stdout, _ = process.communicate()
+    stdout = stdout.decode("utf8")
+    with open("./USAGE") as f:
+        usage = f.read()
+
+    self.assertEquals(stdout, usage)
+
+    def test_upper_short(self):
+        args = ["-u", "hello world"]
+        ns = self.parser.parse_args(args)
+        result = echo.main(args)
+        expected = "HELLO WORLD"
+
+        self.assertTrue(ns.upper)
+        self.assertEqual(result, expected)
+
+    def test_lower_short(self):
+        args = ["-l", "HELLO WORLD"]
+        ns = self.parser.parse_args(args)
+        result = echo.main(args)
+        expected = "hello world"
+
+        self.assertTrue(ns.lower)
+        self.assertEqual(result, expected)
+
+    def test_title_short(self):
+        args = ["-t", "hello world"]
+        ns = self.parser.parse_args(args)
+        result = echo.main(args)
+        expected = "Hello World"
+
+        self.assertTrue(ns.title)
+        self.assertEqual(result, expected)
+
+    def test_upper_long(self):
+        args = ["--upper", "hello world"]
+        ns = self.parser.parse_args(args)
+        result = echo.main(args)
+        expected = "HELLO WORLD"
+
+        self.assertTrue(ns.upper)
+        self.assertEqual(result, expected)
+
+    def test_lower_long(self):
+        args = ["--lower", "HELLO WORLD"]
+        ns = self.parser.parse_args(args)
+        result = echo.main(args)
+        expected = "hello world"
+
+        self.assertTrue(ns.lower)
+        self.assertEqual(result, expected)
+
+    def test_title_long(self):
+        args = ["--title", "hello world"]
+        ns = self.parser.parse_args(args)
+        result = echo.main(args)
+        expected = "Hello World"
+
+        self.assertTrue(ns.title)
+        self.assertEqual(result, expected)
+
+    def test_all(self):
+        args = ["-tul", "HeLlO wOrLd"]
+        ns = self.parser.parse_args(args)
+        self.assertTrue(ns.upper)
+        self.assertTrue(ns.lower)
+        self.assertTrue(ns.title)
+        actual = echo.main(args)
+        expected = "Hello World"
+        self.assertEqual(actual, expected)
+
+    def test_no_args(self):
+        args = ["hello world"]
+        ns = self.parser.parse_args(args)
+        result = echo.main(args)
+        expected = "hello world"
+
+        self.assertTrue(ns)
+        self.assertEqual(result, expected)
+
 
 if __name__ == '__main__':
     unittest.main()
